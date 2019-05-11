603939e06904a332d5913d3f4685a1135dd55e4f
EC2 Default User <ec2-user@ip-10-0-0-73.us-west-2.compute.internal>
2019-05-11 18:10:59 +0000

    Fix for CMS Blog Template not rendering markdown.

--- src/cms/preview-templates/ArticlePreview.js
+++ src/cms/preview-templates/ArticlePreview.js
@@ -4,14 +4,20 @@ import ArticleTemplate from '../../components/ArticleTemplate'
 
 const ArticlePreview = ({entry, widgetFor}) => {
   return (
-    <ArticleTemplate
-      content={widgetFor('body')}
-      cover={entry.getIn(['data', 'cover'])}
-      meta_title={entry.getIn(['data', 'meta_title'])}
-      meta_desc={entry.getIn(['data', 'meta_description'])}
-      title={entry.getIn(['data', 'title'])}
-      slug={entry.getIn(['data', 'slug'])}
-    />
+    <div className='container content'>
+      <div className='columns'>
+        <div className='column is-10 is-offset-1'>    
+          <ArticleTemplate
+            content={widgetFor('body')}
+            cover={entry.getIn(['data', 'cover'])}
+            meta_title={entry.getIn(['data', 'meta_title'])}
+            meta_desc={entry.getIn(['data', 'meta_description'])}
+            title={entry.getIn(['data', 'title'])}
+            slug={entry.getIn(['data', 'slug'])}
+          />
+        </div>
+      </div>
+    </div>
   )
 }
 
